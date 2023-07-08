1bc5ae0e71e3b085e6ffc636735a383e99ccd194
Nodrexa <nodo.imedashvili@gmail.com>
2023-07-08 15:14:03 +0400

    add users

---
+++ src/db/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    firstName: 'Cucumber',
+    balance: 1000,
+    isDeleted: false,
+  },
+  {
+    id: 2,
+    firstName: 'Tomato',
+    balance: 2000,
+    isDeleted: false,
+  },
+  {
+    id: 3,
+    firstName: 'Broccoli',
+    balance: 300,
+    isDeleted: false,
+  },
+  {
+    id: 4,
+    firstName: 'Lettuce',
+    balance: 100,
+    isDeleted: false,
+  },
+]
